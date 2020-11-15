d8341c4c0a0d50e33cae867f9ffa523820335886
hi <kangja1996@gmail.com>
2020-11-15 16:02:30 -0500

    progress

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,5 +1,11 @@
 // import the React and React DOM libraries
+import React from "react";
+import ReactDOM from "react-dom";
 
 // Create a react component 
 
 // Take the react component and show it on the screen 
+ReactDOM.render(
+  <App />,
+  document.querySelector("#root")
+);
