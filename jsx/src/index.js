b18da41ed925ec2eca84d22f7ad304e0a856d1f7
hi <kangja1996@gmail.com>
2020-11-16 22:55:29 -0500

    progress

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -4,11 +4,12 @@ import ReactDOM from "react-dom";
 
 // Create a react component 
 const App = () => {
+  const buttonText = ["Hi", "There"];
   return (
     <div>
       <label className="label" for="name">Enter name:</label>
       <input id="name" type="text"  />
-      <button style={{ backgroundColor: "blue", color:"white" }}> Submit</button>
+      <button style={{ backgroundColor: "blue", color: "white" }}>{buttonText}</button>
     </div>
   )
 };
